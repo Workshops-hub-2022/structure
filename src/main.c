b7a19414d3545dd55dc203e32ac41fe925a430bf
Nathan <nathan.barre@epitech.eu>
2022-11-17 11:28:49 +0100

    solution

--- src/main.c
+++ src/main.c
@@ -9,12 +9,32 @@
 
 bool is_number(char *str)
 {
+    int index = 0;
+
+    while (str[index] != '\0') {
+        if (str[index] <= '0' || str[index] >= '9') {
+            return (false);
+        }
+        index++;
+    }
+    return (true);
 }
 
 bool is_error_in_arg(int ac, char **av)
 {
+    if (ac != 2)
+        return (true);
+    if (is_number(av[1]))
+        return (true);
+    return (false);
 }
 
 int main(int ac, char **av)
 {
+    if (is_error_in_arg(ac, av))
+        return (84);
+    warrior_t *warrior = new_warrior(av[1]);
+    arena(warrior);
+    free_warrior(warrior);
+    return(0);
 }
